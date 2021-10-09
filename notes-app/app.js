8447c0b31541a6fdf220e8742becd86c5579ebfd
gkaragoz <gokhankaragoz@gameguru.net>
2021-10-09 13:37:18 +0300

    Implement readNote

--- notes-app/app.js
+++ notes-app/app.js
@@ -55,8 +55,15 @@ yargs.command({
 yargs.command({
 	command: "read",
 	describe: "Read a note",
-	handler: function () {
-		console.log("Reading the note!");
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: function (argv) {
+		notes.readNote(argv.title);
 	},
 });
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -43,6 +43,18 @@ const listNotes = () => {
 	});
 };
 
+const readNote = (title) => {
+	const notes = loadNotes();
+	const noteToRead = notes.find((note) => note.title === title);
+
+	if (noteToRead) {
+		console.log(chalk.green.inverse(noteToRead.title));
+		console.log(chalk.green.inverse(noteToRead.body));
+	} else {
+		console.log(chalk.red.inverse("No note found!"));
+	}
+};
+
 const saveNotes = (notes) => {
 	const dataJSON = JSON.stringify(notes);
 	fs.writeFileSync("notes.json", dataJSON);
@@ -64,4 +76,5 @@ module.exports = {
 	addNote: addNote,
 	removeNote: removeNote,
 	listNotes: listNotes,
+	readNote: readNote,
 };
